ee83dfe259a48b7593005dd22a816bd492730eb7
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2017-05-29 23:58:43 +0200

    fixed time slot calculation

--- XML_parser.py
+++ XML_parser.py
@@ -19,7 +19,7 @@ def read_schedule(url):
     context["title"] = con_info.find("title").text
     context["timeslot"] = con_info.find("timeslot_duration").text
     time_slot_mins = int(context["timeslot"].split(":")[1])
-    time_slot_hours = 60 / int(context["timeslot"].split(":")[1])
+    time_slot_hours = int(context["timeslot"].split(":")[0])
     #get the days
     days = root.findall("day")
     #define the temporary variables for collect all information
@@ -33,13 +33,40 @@ def read_schedule(url):
         diff_days = 0
         if start_day < end_day:
             diff_days = end_day - start_day
-        start_times = day.attrib["start"].split("T")[1].split("+")[0].split(":")
-        end_times = day.attrib["end"].split("T")[1].split("+")[0].split(":")
-        total_time_hours = (diff_days*24) + int(end_times[0]) - int(start_times[1])
-        total_time_mins = (total_time_hours*60) + int(end_times[1]) - int(start_times[1])
+        start_time = day.attrib["start"].split("T")[1].split("+")[0].split(":")
+        end_time = day.attrib["end"].split("T")[1].split("+")[0].split(":")
+        #calculate the hours and minutes of the time slots and use this to get total number of time slots
+        total_time_hours = (diff_days*24) + int(end_time[0]) - int(start_time[1])
+        total_time_mins = (total_time_hours*60) + int(end_time[1]) - int(start_time[1])
         total_time_slots = total_time_mins/time_slot_mins
+        #get the start time
+        time_slots = [start_time[0] + ":" + start_time[1]]
+        #calculate the time of the time slots
+        temp_time_slot = [int(start_time[0]), int(start_time[1])]
+        while temp_time_slot[0] is not int(end_time[0]) or temp_time_slot[1] is not int(end_time[1]):
+            #check if the next hour is reached when the next slot in minutes is added
+            # if so, add another hour and remove this hour from the minutes 
+            if not ((int(temp_time_slot[1]) + int(time_slot_mins)) - 60) < 0:
+                temp_time_slot[0] += 1
+                temp_time_slot[1] -= 60
+            #check if another day begins
+            # if so, remove 24 hours
+            if not ((int(temp_time_slot[0]) + int(time_slot_hours)) - 24) < 0:
+                temp_time_slot[0] -= 24
+            #add a time slot
+            temp_time_slot[0] += int(time_slot_hours)
+            temp_time_slot[1] += int(time_slot_mins)
+            #if the minutes are zero, still use two digits
+            if temp_time_slot[1] == 0:
+                temp_time_slot[1] = "00"
+            else:
+                temp_time_slot[1] = str(temp_time_slot[1])
+            #save the new time slot
+            time_slots.append(str(temp_time_slot[0]) + ":" + temp_time_slot[1])
+            #turn the minutes back into a number
+            temp_time_slot[1] = int(temp_time_slot[1])
         #put all information into a dictionary to use later
-        temp_days.append({"date":day.attrib["date"], "rooms":[], "start":start_times, "end":end_times, "total_time_slots":total_time_slots})
+        temp_days.append({"date":day.attrib["date"], "rooms":[], "start":start_time, "end":end_time, "time_slots": time_slots, "total_time_slots":total_time_slots})
         #get the rooms
         rooms = day.findall("room")
         for room in rooms:
@@ -59,7 +86,11 @@ def read_schedule(url):
                 #get the number of time slots this event/talk
                 durations = event.find("duration").text.split(":")
                 #calculate the amount of timeslots this event uses
-                temp_event["time_slots"] = (int(durations[0]) * time_slot_hours) + (int(durations[1]) / time_slot_mins)
+                if time_slot_hours is not 0:
+                    temp_event["time_slots"] = (int(durations[0]) * time_slot_hours) + (int(durations[1]) / time_slot_mins)
+                else:
+                    temp_event["time_slots"] = (int(durations[0]) * 60 + int(durations[1])) / time_slot_mins
+                temp_event["start"] = event.find("start").text
                 temp_room["events"].append(temp_event)
             temp_days[counter]["rooms"].append(temp_room)
         #increase the counter
