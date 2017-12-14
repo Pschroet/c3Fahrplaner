1c8257a7bd9467fbf1a702be3a100e8641f91216
Philipp Schroeter <philipp.schroeter.1@hu-berlin.de>
2017-12-14 22:50:05 +0100

    added local test files and updated main to use the 2017 c3

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import fahrplan_writer
 import util
 import file_reader
-#import web_file_reader
+import web_file_reader
 
 if __name__ == '__main__':
     #get the schedule from a file
-    file_name = "schedule.xml"
-    context = file_reader.read_schedule(file_name)
+    #file_name = "33c3-schedule.xml"
+    #file_name = "PrivacyWeek-schedule.xml"
+    #context = file_reader.read_schedule(file_name)
     #get the schedule from a URL
     #url = "https://fahrplan.events.ccc.de/congress/2016/Fahrplan/schedule.xml"
-    #context = web_file_reader.read_schedule(url)
+    url = "https://events.ccc.de/congress/2017/Fahrplan/schedule.xml"
+    context = web_file_reader.read_schedule(url)
     #print context
     tmpl = util.readFileContentAsString('fahrplan_template.html')
     parser = fahrplan_writer.fahrplan_writer()
     parser.set_context(context)
     parser.feed(tmpl)
-    destination = "fahrplan.html"
+    destination = "/tmp/fahrplan.html"
     print "Writing to '" + destination + "'"
     util.write2File(destination, parser.get_result(), "w")
