7324e919ada6563c1841a9a0c057c611f2861a1b
Philipp Schroeter <philipp-schroeter@gmx.net>
2019-09-22 15:35:44 +0200

    added a default function to create the events for the fahrplan, if an event other than the supported ones is parsed and changed the layout of the event code creation to make it easier to read (it's not just a single long line of code anymore)

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 This software is released under the GNU General Public License v3.0 (see LICENSE).
 
-This is a little tool to create a webpage containing the fahrplan of the Chaos Communication Congress.
+This is a little tool to create a webpage containing the fahrplan of the Chaos Communication Congress, the Chaos Communication Camp and Datenspuren.
 The events are clickable and everything needed to work is a web server (for the cookies, the rest should work even if opened as a file).
 
 To start the script just run
@@ -13,6 +13,6 @@ To start the script just run
 python main.py
 ```
 
-At the moment, this processes the fahrplan for the 34c3. To change this, modify the file to be parsed in _main.py_ to either a local file or a remote one.
+At the moment, this processes the fahrplan for the Datenspuren 2019. To change this, modify the file to be parsed in _main.py_ to either a local file or a remote one.
 
-The tool can be used to process other schedules in XML the same format as the fahrplans, but the links won't work, although the events should.
+The tool can be used to process other schedules in XML the same format as the fahrplans, but there won't be any links, although the events should be shown.
--- fahrplan_writer.py
+++ fahrplan_writer.py
@@ -15,14 +15,14 @@ class fahrplan_writer(HTMLParser):
         self.event_types = {}
         self.event_types["cccongress"] = {"title":"Chaos Communication Congress",
                                           "function_name":"cccongress"}
-        self.event_types["cccongress"]["regex"] = re.compile(self.event_types["cccongress"]["title"], re.IGNORECASE)
+        self.event_types["cccongress"]["regex"] = re.compile(".*" + self.event_types["cccongress"]["title"] + ".*", re.IGNORECASE)
         self.event_types["cccamp"] = {"title":"Chaos Communication Camp",
                                       "function_name":"cccamp"}
-        self.event_types["cccamp"]["regex"] = re.compile(self.event_types["cccamp"]["title"], re.IGNORECASE)
+        self.event_types["cccamp"]["regex"] = re.compile(".*" + self.event_types["cccamp"]["title"] + ".*", re.IGNORECASE)
         self.event_types["dspuren"] = {"title":"Datenspuren",
                                        "function_name":"dspuren"}
-        self.event_types["dspuren"]["regex"] = re.compile(self.event_types["dspuren"]["title"], re.IGNORECASE)
-        self.event_function = None
+        self.event_types["dspuren"]["regex"] = re.compile(".*" + self.event_types["dspuren"]["title"] + ".*", re.IGNORECASE)
+        self.event_function_name = "other_event"
 
     def set_context(self, context):
         self.context = context
@@ -40,15 +40,20 @@ class fahrplan_writer(HTMLParser):
                 self.fahrplan += " " + attr[0] + "='" + attr[1] + "'"
             self.fahrplan += ">" + self.context["title"]
             for event_type in self.event_types:
+                #print(str(event_type))
+                #print(str(self.event_types[event_type]))
+                #print(str(self.context["title"]))
+                #print(self.event_types[event_type]["regex"].match(self.context["title"]))
                 if self.event_types[event_type]["regex"].match(self.context["title"]):
-                    self.event_function = self.event_types[event_type]["function_name"]
+                    self.event_function_name = self.event_types[event_type]["function_name"]
         elif tag == "h1":
             self.fahrplan += "<h1>" + self.context["title"]
         elif tag == "p":
-            if self.event_function is not None:
-                self.fahrplan += getattr(self, self.event_function)(tag)
+            if self.event_function_name is not None:
+                self.fahrplan += getattr(self, self.event_function_name)(tag)
         #add the information about the events at the days
         elif tag == "div":
+            #print(str(self.event_function_name))
             day_events = "<div class='day'>"
             for day in self.context["days"]:
                 day_events += "<h3>" + day["date"]  + "</h3>\n"
@@ -74,8 +79,8 @@ class fahrplan_writer(HTMLParser):
                         #print time_slot + " == " + events[last_event]["start"]
                         #if not, go on
                         if time_slot == events[last_event]["start"] or "0" + time_slot == events[last_event]["start"]:
-                            if self.event_function is not None:
-                                day_events += getattr(self, self.event_function)(tag, events[last_event]["time_slots"], events[last_event]["id"], events[last_event]["title"])
+                            if self.event_function_name is not None:
+                                day_events += getattr(self, self.event_function_name)(tag, events[last_event]["time_slots"], events[last_event]["id"], events[last_event]["title"])
                             #calculate the columns used, subtract the current table field
                             colspan = events[last_event]["time_slots"] - 1
                             #print "-> " + str(events[last_event])
@@ -116,21 +121,56 @@ class fahrplan_writer(HTMLParser):
     def handle_comment(self, data):
         self.fahrplan += "<!-- " + data + " -->"
 
+    def other_event(self, tag, event_time_slots="", event_id="", event_title=""):
+        if tag == "p":
+            return ""
+        elif tag == "div":
+            event_code = "<td class='something' colspan='" + str(event_time_slots) + "'"
+            event_code += " onclick='toggleClick(this, false);'"
+            event_code += " id='" + event_id + "'"
+            event_code += " title='unselected'></td>\n"
+            return event_code
+
     def cccongress(self, tag, event_time_slots="", event_id="", event_title=""):
         if tag == "p":
-            useful_links = "<p><a href='https://events.ccc.de/tag/" + self.context["acronym"] + "'>Event blog</a></p>"
-            useful_links += "<p><a href='https://events.ccc.de/congress/" + self.context["year"] + "/wiki/'>Wiki</a>"
+            useful_links = "<p><a href='https://events.ccc.de/tag/" + self.context["acronym"]
+            useful_links += "'>Event blog</a></p>"
+            useful_links += "<p><a href='https://events.ccc.de/congress/" +  self.context["year"]
+            useful_links += "/wiki/'>Wiki</a>"
             return useful_links
         elif tag == "div":
-            return "<td class='something' colspan='" + str(event_time_slots) + "' onclick='toggleClick(this, false);' " + "id='" + event_id + "' title='unselected'><a href='https://media.ccc.de/tags/" + event_id + "'>" + event_title + "</a> (<a href='https://fahrplan.events.ccc.de/congress/" + self.context["year"] + "/Fahrplan/events/" + event_id + ".html' onmouseover='onLink=true;' onmouseout='onLink=false;' target='_blank'>--></a>)" + "</td>\n"
+            event_code = "<td class='something' colspan='" + str(event_time_slots) + "'"
+            event_code += " onclick='toggleClick(this, false);'"
+            event_code += " id='" +  event_id + "'"
+            event_code += " title='unselected'>"
+            event_code += "<a href='https://media.ccc.de/tags/" + event_id + "'>"
+            event_code += event_title
+            event_code += "</a>"
+            event_code += " (<a href='https://fahrplan.events.ccc.de/congress/" + self.context["year"]
+            event_code += "/Fahrplan/events/" +  event_id + ".html'"
+            event_code += " onmouseover='onLink=true;' onmouseout='onLink=false;' target='_blank'>--></a>)" + "</td>\n"
+            return event_code
 
     def cccamp(self, tag, event_time_slots="", event_id="", event_title=""):
         if tag == "p":
-            useful_links = "<p><a href='https://events.ccc.de/category/camp/camp-" + self.context["year"] + "'>Event blog</a></p>"
-            useful_links += "<p><a href='https://events.ccc.de/camp/" + self.context["year"] + "/wiki/'>Wiki</a>"
+            useful_links = "<p><a href='https://events.ccc.de/category/camp/camp-"
+            useful_links += self.context["year"]
+            useful_links += "'>Event blog</a></p>"
+            useful_links += "<p><a href='https://events.ccc.de/camp/"
+            useful_links += self.context["year"]
+            useful_links += "/wiki/'>Wiki</a>"
             return useful_links
         elif tag == "div":
-            return "<td class='something' colspan='" + str(event_time_slots) + "' onclick='toggleClick(this, false);' " + "id='" + event_id + "' title='unselected'><a href='https://media.ccc.de/tags/" + event_id + "'>" + event_title + "</a> (<a href='https://fahrplan.events.ccc.de/camp/" + self.context["year"] + "/Fahrplan/events/" + event_id + ".html' onmouseover='onLink=true;' onmouseout='onLink=false;' target='_blank'>--></a>)" + "</td>\n"
+            event_code = "<td class='something' colspan='" + str(event_time_slots) + "'"
+            event_code += " onclick='toggleClick(this, false);'"
+            event_code += " id='" + event_id + "'"
+            event_code += " title='unselected'><a href='https://media.ccc.de/tags/" + event_id + "'>"
+            event_code += event_title
+            event_code += "</a>"
+            event_code += " (<a href='https://fahrplan.events.ccc.de/camp/" + self.context["year"]
+            event_code += "/Fahrplan/events/" + event_id + ".html'"
+            event_code += " onmouseover='onLink=true;' onmouseout='onLink=false;' target='_blank'>--></a>)" + "</td>\n"
+            return event_code
 
     def dspuren(self, tag, event_time_slots="", event_id="", event_title=""):
         if tag == "p":
@@ -138,4 +178,13 @@ class fahrplan_writer(HTMLParser):
             useful_links += "<p><a href='https://wiki.c3d2.de/Datenspuren/" + self.context["year"] + "'>Wiki</a>"
             return useful_links
         elif tag == "div":
-            return "<td class='something' colspan='" + str(event_time_slots) + "' onclick='toggleClick(this, false);' " + "id='" + event_id + "' title='unselected'><a href='https://media.ccc.de/tags/" + event_id + "'>" + event_title + "</a> (<a href='https://datenspuren.de/" + self.context["year"] + "/fahrplan/events/" + event_id + ".html' onmouseover='onLink=true;' onmouseout='onLink=false;' target='_blank'>--></a>)" + "</td>\n"
+            event_code = "<td class='something' colspan='" + str(event_time_slots) + "'"
+            event_code += " onclick='toggleClick(this, false);'"
+            event_code += " id='" + event_id + "'"
+            event_code += " title='unselected'><a href='https://media.ccc.de/tags/" + event_id + "'>"
+            event_code += event_title
+            event_code += "</a>"
+            event_code += " (<a href='https://datenspuren.de/" + self.context["year"]
+            event_code += "/fahrplan/events/" + event_id + ".html'"
+            event_code += " onmouseover='onLink=true;' onmouseout='onLink=false;' target='_blank'>--></a>)" + "</td>\n"
+            return event_code
